b6f2a414e047ed4df16180b9a27ef0173e4a2cbb
Dani Hidayat <46246911+DaniHidayat@users.noreply.github.com>
2024-11-24 21:27:15 +0700

    add landing page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Pengajuan Anggaran Kemenag</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo.png') }}" type="image/x-icon">
+    <link href="{{ asset('assets/css/landing.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Background Video-->
+    <img class="bg-image" src="{{ asset('assets/images/kantor.jpg') }}">
+    <!-- Masthead-->
+    <div class="masthead">
+        <div class="masthead-content text-white">
+            <div class="container-fluid px-4 px-lg-0">
+                <h1 class="lh-1 mb-4 text-black">Pengajuan Anggaran Kemenag</h1>
+                <p class="mb-5 text-black">Aplikasi yang memberikan fasilitas dan informasi terkait dengan Proses Pengajuan Anggaran</p>
+                <!-- Email address input-->
+                <div class="row input-group-newsletter">
+                    <div class="col-auto mb-2">
+                        <a href="{{ route('login') }}"><button class="btn btn-danger btn-sm text-black" type="button">Login</button></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
